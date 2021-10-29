d0687ad687e50191e030cdf1e95350d9e7ac2173
Hui Yang <chy1988@antwebstudio.com>
2021-10-29 18:54:50 +0800

    add LoaderModa, helper to manage admin layout - work with AdminLayoutManager add crud helper to load Edit, Create, SharedForm pages

---
+++ helpers/crud.js
@@ -0,0 +1,96 @@
+import Form from '@/services/Form'
+import crudMixin from 'fusioncms-helper-js/mixins/form'
+
+export { crudMixin }
+
+export let sharedFormMixin = {
+    props: {
+        entry: {
+            required: false
+        },
+
+        extension: {
+            required: true,
+        },
+
+        form: {
+            type: Object,
+            required: true,
+        }
+    },
+}
+
+function init(vm, response, fields, dataCallback) {
+    if (typeof(dataCallback) == 'function') {
+        dataCallback(vm, response)
+    } else {
+        vm.extension = response.data.data.extension || {}
+        vm.entry = response.data.data.entry || {}
+    }
+
+    fields = _.merge(vm.entry, fields)
+
+    if (vm.extension && vm.extension.fieldset) {
+        _.forEach(extension.fieldset.sections, function(section) {
+            _.forEach(section.fields, function(field) {
+                fields[field.handle] = entry[field.handle] || null
+            })
+        })
+    }
+
+
+    vm.form = new Form(fields, true)
+}
+
+export function fieldsetToSection(fieldset) {
+    let body = []
+    let sidebar = []
+
+    if (fieldset) {
+        body = _.filter(fieldset.sections, (section) =>
+            section.placement == 'body')
+
+        sidebar = _.filter(fieldset.sections, (section) =>
+            section.placement == 'sidebar')
+    }
+
+
+    return { body, sidebar }
+}
+
+export function loadCreateBeforeRouteEnter(url, next, fields, dataCallback) {
+    axios.get(url).then((response) => {
+
+        next((vm) => {
+            init(vm, response, fields, dataCallback)
+        })
+    })
+}
+
+export function loadCreateBeforeRouteUpdate(url, next, fields, dataCallback) {
+    axios.get(url).then((response) => {
+        init(this, response, fields, dataCallback)
+        
+        next()
+    })
+
+}
+
+export function loadEditBeforeRouteEnter(url, next, fields, dataCallback) {
+    axios.get(url).then((response) => {
+
+        next((vm) => {
+            init(vm, response, fields, dataCallback)
+        })
+    })
+
+}
+
+export function loadEditBeforeRouteUpdate(url, next, fields, dataCallback) {
+    axios.get(url).then((response) => {
+        init(this, response, fields, dataCallback)
+        
+        next()
+    })
+
+}
